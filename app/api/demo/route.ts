e506d48e6cd4bb0c9f585d54952e63fa4419d149
mohammad-al-asad <maasad11914@gmail.com>
2025-12-05 09:27:47 +0600

    hdfh

--- app/api/demo/route.ts
+++ app/api/demo/route.ts
@@ -6,26 +6,14 @@ export async function POST() {
 
     // Record the price adjustment
     const updated = await prisma.user.update({
-<<<<<<< HEAD
-      where:{
-        email:"editzsam305@gmail.com"
-      },
-      data: {
-        role:"ADMIN",
-=======
       where: { email: "ma@ma.ma" },
       data: {
         role: "ADMIN",
->>>>>>> 6e96554fd31c3a358adaa7a38b84517eabbd44c4
       },
     });
     return Response.json({
       success: true,
-<<<<<<< HEAD
-      updated,
-=======
       updated
->>>>>>> 6e96554fd31c3a358adaa7a38b84517eabbd44c4
     });
   } catch (error) {
     console.log(error);
